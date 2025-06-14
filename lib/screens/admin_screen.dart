c6ae2248674b332a32d58ec43db887bf79a34229
Thariq Ivan <108126947+vannndar@users.noreply.github.com>
2025-06-14 16:56:41 +0700

    Add: update transaction data

--- lib/screens/admin_screen.dart
+++ lib/screens/admin_screen.dart
@@ -8,6 +8,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:http/http.dart' as http;
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'dart:convert';
+import 'package:intl/intl.dart';
 
 class AdminScreen extends StatefulWidget {
   final String adminEmail;
@@ -54,6 +55,136 @@ class _AdminScreenState extends State<AdminScreen> {
 
     _movieTitleCache[movieId] = future; 
     return future;
+  }
+
+  String formatNumber(int number) {
+    return number.toString().replaceAllMapped(RegExp(r'\B(?=(\d{3})+(?!\d))'), (match) => '.');
+  }
+
+
+  void _showUpdateDialog(TransactionModel tx) {
+    TextEditingController nameController = TextEditingController(text: tx.name);
+    TextEditingController phoneController = TextEditingController(text: tx.phone);
+    TextEditingController addressController = TextEditingController(text: tx.address);
+    TextEditingController totalPriceController = TextEditingController(text: tx.totalPrice.toString());
+    TextEditingController startDateController = TextEditingController(text: tx.startDate);
+    TextEditingController endDateController = TextEditingController(text: tx.endDate);
+
+    String formatDate(DateTime date) {
+      final DateFormat formatter = DateFormat('dd/MM/yyyy');
+      return formatter.format(date);
+    }
+
+    // Function to pick a date and update the corresponding TextField
+    Future<void> _selectDate(BuildContext context, TextEditingController controller) async {
+      final DateTime currentDate = DateTime.now();
+      // apabila endDateController maka
+      final DateTime? pickedDate = await showDatePicker(
+        context: context,
+        initialDate: DateFormat('dd/MM/yyyy').parse(controller.text),
+        firstDate: currentDate,
+        lastDate: DateTime(2101),
+        builder: (BuildContext context, Widget? child) {
+          return Theme(
+            data: ThemeData.dark().copyWith(
+              primaryColor: Colors.blueAccent, // Change the primary color
+              hintColor: Colors.blueAccent, // Change the accent color
+              colorScheme: ColorScheme.dark(primary: Colors.blueAccent), // Change the color scheme
+              buttonTheme: ButtonThemeData(textTheme: ButtonTextTheme.primary), // Change button text color
+            ),
+            child: child!,
+          );
+        },
+      );
+      if (pickedDate != null && pickedDate != currentDate) {
+        controller.text = formatDate(pickedDate); // Update the text field with selected date
+      }
+    }
+
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Update Transaction'),
+          content: SingleChildScrollView(
+            child: Column(
+              children: [
+                TextField(
+                  controller: nameController,
+                  decoration: const InputDecoration(labelText: 'Name'),
+                ),
+                TextField(
+                  controller: phoneController,
+                  decoration: const InputDecoration(labelText: 'Phone'),
+                ),
+                TextField(
+                  controller: addressController,
+                  decoration: const InputDecoration(labelText: 'Address'),
+                ),
+                TextField(
+                  controller: totalPriceController,
+                  decoration: const InputDecoration(labelText: 'Total Price'),
+                ),
+                GestureDetector(
+                  onTap: () => _selectDate(context, startDateController),
+                  child: AbsorbPointer(
+                    child: TextField(
+                      controller: startDateController,
+                      decoration: const InputDecoration(labelText: 'Start Date'),
+                    ),
+                  ),
+                ),
+                GestureDetector(
+                  onTap: () => _selectDate(context, endDateController),
+                  child: AbsorbPointer(
+                    child: TextField(
+                      controller: endDateController,
+                      decoration: const InputDecoration(labelText: 'End Date'),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop(); // Close the dialog
+              },
+              child: Container(
+                padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                decoration: BoxDecoration(
+                  color: Colors.red[700],
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                child: const Text('Cancel', style: TextStyle(color: Colors.white)),
+              ),
+            ),
+            TextButton(
+              onPressed: () async {
+                // Save the updated data back to Firestore
+                await FirebaseFirestore.instance.collection('transactions').doc(tx.id).update({
+                  'name': nameController.text,
+                  'totalPrice': int.tryParse(totalPriceController.text) ?? 0,
+                  'startDate': startDateController.text,
+                  'endDate': endDateController.text,
+                });
+
+                Navigator.of(context).pop(); // Close the dialog
+              },
+              child: Container(
+                padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                decoration: BoxDecoration(
+                  color: Colors.blue[700],
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                child: const Text('Update', style: TextStyle(color: Colors.white)),
+              ),
+            ),
+          ],
+        );
+      },
+    );
   }
 
   @override
@@ -155,56 +286,86 @@ class _AdminScreenState extends State<AdminScreen> {
               final tx = transactions[index];
               return Card(
                 margin: const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
-                child: ListTile(
-                  title: FutureBuilder<String>(
-                    future: _getMovieTitle(tx.movieId),
-                    builder: (context, movieTitleSnapshot) {
-                      if (movieTitleSnapshot.connectionState == ConnectionState.waiting) {
-                        return const Text('Loading movie...');
-                      }
-                      if (movieTitleSnapshot.hasError) {
-                        return Text('Error: ${movieTitleSnapshot.error}');
-                      }
-                      return Text(movieTitleSnapshot.data ?? 'Unknown Movie');
-                    },
-                  ),
-                  subtitle: Column(
+                child: Padding(
+                  padding: const EdgeInsets.all(12.0),
+                  child: Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
+                      // Row for Title and Dropdown
+                      Row(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Expanded(
+                            child: FutureBuilder<String>(
+                              future: _getMovieTitle(tx.movieId),
+                              builder: (context, movieTitleSnapshot) {
+                                if (movieTitleSnapshot.connectionState == ConnectionState.waiting) {
+                                  return const Text('Loading movie...', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16));
+                                }
+                                if (movieTitleSnapshot.hasError) {
+                                  return Text('Error: ${movieTitleSnapshot.error}', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16));
+                                }
+                                return Text(
+                                  movieTitleSnapshot.data ?? 'Unknown Movie',
+                                  style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+                                );
+                              },
+                            ),
+                          ),
+                          const SizedBox(width: 8),
+                          DropdownButton<String>(
+                            value: tx.status,
+                            items: [
+                              DropdownMenuItem(
+                                value: 'waiting for confirmation', 
+                                child: Text('Waiting for confirmatio', style: TextStyle(color: Colors.yellow[700]))),
+                              DropdownMenuItem(
+                                value: 'on rent', 
+                                child: Text('On Rent', style: TextStyle(color: Colors.green[700]))),
+                              DropdownMenuItem(
+                                value: 'returned', 
+                                child: Text('Returned', style: TextStyle(color: Colors.blue[700]))),
+                              DropdownMenuItem(
+                                value: 'lost', 
+                                child: Text('Lost', style: TextStyle(color: Colors.red[700]))),
+                            ],
+                            onChanged: (value) {
+                              if (value != null) {
+                                FirebaseFirestore.instance.collection('transactions').doc(tx.id).update({'status': value});
+                              }
+                            },
+                          ),
+                        ],
+                      ),
+                      const SizedBox(height: 8),
+
+                      // Transaction Details
                       Text('User: ${tx.name}'),
+                      const SizedBox(height: 4),
                       Text('Status: ${tx.status}'),
-                      Text('Total: ${tx.totalPrice}'),
+                      const SizedBox(height: 4),
+                      Text('Total: Rp ${formatNumber(tx.totalPrice)}'),
+                      const SizedBox(height: 4),
                       Text('Tanggal: ${tx.startDate} - ${tx.endDate}'),
-                    ],
-                  ),
-                  trailing: DropdownButton<String>(
-                    value: tx.status,
-                    items: [
-                      DropdownMenuItem(
-                        value: 'waiting for confirmation',
-                        child: Text('Waiting for Confirmation', style: TextStyle(color: Colors.yellow[700])),
-                      ),
-                      DropdownMenuItem(
-                        value: 'on rent',
-                        child: Text('On Rent', style: TextStyle(color: Colors.green[700])),
-                      ),
-                      DropdownMenuItem(
-                        value: 'returned',
-                        child: Text('Returned', style: TextStyle(color: Colors.blue[700])),
-                      ),
-                      DropdownMenuItem(
-                        value: 'lost',
-                        child: Text('Lost', style: TextStyle(color: Colors.red[700])),
+                      const SizedBox(height: 4),
+
+                      // Update Button aligned to the right
+                      Align(
+                        alignment: Alignment.centerRight,
+                        child: Container(
+                          width: 100,
+                          margin: EdgeInsets.only(bottom: 10),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(20),
+                            color: Colors.blue,
+                          ),
+                          child: TextButton(
+                            onPressed: () => _showUpdateDialog(tx),
+                            child: const Text('Update', style: TextStyle(color: Colors.white)),
+                          ),
+                        ),
                       ),
                     ],
-                    onChanged: (value) {
-                      if (value != null) {
-                        FirebaseFirestore.instance
-                            .collection('transactions')
-                            .doc(tx.id)
-                            .update({'status': value});
-                      }
-                    },
                   ),
                 ),
               );
